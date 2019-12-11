c5c39b855d625314011f4e9f3b8fd1b0a32f7ca3
DhruvilKarani <karanidhruvil@gmail.com>
2019-12-11 06:50:14 +0530

    Add explained variance in PCA

--- unsupervised/PCA.py
+++ unsupervised/PCA.py
@@ -9,8 +9,8 @@ class PCA:
 
     def __init__(self, n_components):
         self.n = n_components
-        self._eigval = None
         self.axes_ = None
+        self.explained_variance_ = None
 
     @staticmethod
     def _covariance_matrix(X):
@@ -22,6 +22,13 @@ class PCA:
         eig_val, eig_vect = eig(self._covariance_matrix(X))
         idxes = (-eig_val).argsort()[:self.n]
         self.axes_ = np.array([eig_vect[:, idx] for idx in idxes])
+        self.explained_variance_ = sorted(eig_val, reverse=True)
         
-    def transform(self, X)
+    def transform(self, X):
+        return np.matmul(self.axes_.T, X)
+    
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
+    
 
